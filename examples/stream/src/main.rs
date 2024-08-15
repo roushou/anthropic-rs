9b9a3a2db93608dbfabfeab9ee8720a6b8d2cf40
roushou <roushou9@gmail.com>
2024-08-16 04:45:58 +0700

    feat: add examples

---
+++ examples/stream/src/main.rs
@@ -0,0 +1,31 @@
+use anthropic_rs::{
+    api::message::{Content, ContentType, Message, MessageRequest, Role},
+    client::Client,
+    config::Config,
+    models::model::Model,
+};
+
+#[tokio::main]
+async fn main() {
+    let api_key = std::env::var("ANTHROPIC_API_KEY")
+        .expect("environment variable ANTHROPIC_API_KEY should be defined");
+
+    let config = Config::new(api_key);
+    let client = Client::new(config).unwrap();
+
+    let message = MessageRequest {
+        model: Model::Claude35Sonnet,
+        stream: Some(true),
+        max_tokens: 1024,
+        messages: vec![Message {
+            role: Role::User,
+            content: vec![Content {
+                content_type: ContentType::Text,
+                text: "Hello World".to_string(),
+            }],
+        }],
+        ..Default::default()
+    };
+
+    client.stream_message(message.clone()).await.unwrap();
+}
